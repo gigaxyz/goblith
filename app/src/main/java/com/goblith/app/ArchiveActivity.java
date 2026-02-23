b83d5234e9df1f98d43b77085299d306fa94f1bc
GobliN <barbargoos@gmail.com>
2026-02-23 23:37:18 +0300

    ArchiveActivity ArrayList ve List import eklendi

--- app/src/main/java/com/goblith/app/ArchiveActivity.java
+++ app/src/main/java/com/goblith/app/ArchiveActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArchiveActivity extends AppCompatActivity {
 
